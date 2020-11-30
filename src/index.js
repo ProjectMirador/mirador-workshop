aa69ac669c4cb3931a5c593d27324233a2a3255a
Camille Villa <cvilla100@gmail.com>
2020-11-30 11:52:47 -0800

    Add an elastic mode example

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import Mirador from 'mirador/dist/es/src/index';
 import examplePlugin from './plugins/example_plugin';
 
 const config = {
-  id: 'mirador-viewer',
-  windows: [
-    {
-      manifestId: 'https://purl.stanford.edu/fr426cg9537/iiif/manifest',
+    id: 'mirador-viewer',
+    windows: [{
+            manifestId: 'https://purl.stanford.edu/fr426cg9537/iiif/manifest',
+        },
+        {
+            manifestId: 'https://purl.stanford.edu/qg074bz5077/iiif/manifest',
+        },
+        {
+            manifestId: 'https://purl.stanford.edu/wj335sy2632/iiif/manifest',
+        }
+    ],
+    workspace: {
+        type: 'elastic',
     }
-  ],
 };
 
 const plugins = [];
