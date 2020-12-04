579f0cb50999c8a383a063674ec4a22dab0dc282
Camille Villa <cvilla100@gmail.com>
2020-12-03 19:28:00 -0800

    Add an example with multilingual metadata

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import Mirador from 'mirador/dist/es/src/index';
 import examplePlugin from './plugins/example_plugin';
 
+// An example with a multilingual manifest. Metadata is available in English and German.
 const config = {
-  id: 'mirador-viewer',
-  windows: [
-    {
-      manifestId: 'https://purl.stanford.edu/fr426cg9537/iiif/manifest',
-    }
-  ],
+    id: 'mirador-viewer',
+    language: 'de', // Use German as the default UI language.
+    // If metadata is available in the UI language, it will be displayed in the sidebar
+    windows: [{
+        manifestId: 'https://iiif.hab.de/object/mss_272-helmst/manifest.json',
+    }],
+    window: {
+        sideBarOpen: true,
+        showLocalePicker: true, // The metadata locale picker is hidden by default
+    },
 };
 
 const plugins = [];
