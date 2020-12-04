c221326adecea72e5e20587e1ed4748118989fce
Camille Villa <cvilla100@gmail.com>
2020-12-03 21:38:23 -0800

    Add example with multiple sequences

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import Mirador from 'mirador/dist/es/src/index';
 import examplePlugin from './plugins/example_plugin';
 
 const config = {
-  id: 'mirador-viewer',
-  windows: [
-    {
-      manifestId: 'https://purl.stanford.edu/fr426cg9537/iiif/manifest',
+    id: 'mirador-viewer',
+    windows: [{
+        manifestId: 'https://www.e-codices.unifr.ch/metadata/iiif/gau-Fragment/manifest.json',
+    }],
+    window: {
+        sideBarOpen: true,
+        sideBarPanel: 'canvas',
     }
-  ],
 };
 
 const plugins = [];
